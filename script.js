4fee3ed1e45957bf584bae4dced5f50fcde73666
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-11-21 16:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ function getNumbers() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve([1, 2, 3, 4]);
-    }, 3000);
+    }, 3000); 
   });
 }
 
 getNumbers()
   .then((numbers) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const evenNumbers = numbers.filter((num) => num % 2 === 0);
         document.getElementById("output").textContent = evenNumbers.join(", ");
         resolve(evenNumbers);
-      }, 1000); 
+      }, 1000);
     });
   })
   .then((evenNumbers) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const multipliedNumbers = evenNumbers.map((num) => num * 2);
-        document.getElementById("output").textContent =
-          multipliedNumbers.join(", ");
+        document.getElementById("output").textContent = multipliedNumbers.join(", ");
         resolve(multipliedNumbers);
       }, 2000); 
     });
