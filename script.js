f5d8534736f2797fcf4c5852aabbb95e26749855
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-11-25 11:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-function getNumbers() {
+function generateNumbers() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve([1, 2, 3, 4]);
-    }, 3000); 
+    }, 3000);
   });
 }
 
-async function processNumbers() {
-  try {
-    const numbers = await getNumbers(); 
-
-    const evenNumbers = numbers.filter((num) => num % 2 === 0);
-    setTimeout(() => {
-      document.getElementById("output").textContent = evenNumbers.join(", ");
-    }, 1000); 
-    
-    const multipliedNumbers = evenNumbers.map((num) => num * 2);
-    setTimeout(() => {
-      document.getElementById("output").textContent = multipliedNumbers.join(", ");
-    }, 3000);
-
-  } catch (err) {
-    console.error(err);
-  }
+function filterOddNumbers(numbers) {
+  return numbers.filter((number) => number % 2 === 0);
 }
 
-processNumbers();
+function multiplyEvenNumbers(numbers) {
+  return numbers.map((number) => number * 2);
+}
+
+function updateOutput(text) {
+  const outputElement = document.getElementById('output');
+  outputElement.innerText = text;
+}
+
+generateNumbers()
+  .then((numbers) => {
+    updateOutput(numbers.join(', '));
+    return filterOddNumbers(numbers);
+  })
+  .then((evenNumbers) => {
+    setTimeout(() => {
+      updateOutput(evenNumbers.join(', '));
+    }, 1000);
+    return evenNumbers;
+  })
+  .then((evenNumbers) => {
+    setTimeout(() => {
+      const multipliedNumbers = multiplyEvenNumbers(evenNumbers);
+      updateOutput(multipliedNumbers.join(', '));
+    }, 2000);
+  });
